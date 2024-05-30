cf9f7c4e7468873a3714765bad714343b7d8e157
Rukunuljoy <rukunuljoypub58@gmail.com>
2024-05-31 01:36:24 +0600

    data get is done

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const port = process.env.PORT || 6001;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
 require("dotenv").config();
+
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-//rukunuljoypub58
-//d8xtamzGF5InaG95
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@food-delivery-cluster.0vdi1ak.mongodb.net/?retryWrites=true&w=majority&appName=Food-Delivery-Cluster`
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    //database & collections
+
+    const menuCollection = client.db("foodDelivery").collection("menus")
+    const cartCollection = client.db("foodDelivery").collection("cartItems")
+
+    //all menu items operations
+
+    app.get('/menu', async (req, res) => {
+      const query = {};
+      const cursor = menuCollection.find(query)
+      const services = await cursor.toArray();
+      res.send(services);
+    })
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
